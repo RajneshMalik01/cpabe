403cd5befa58cc37ac9206608b76ca48027e4a63
rajnesh <rajnesh@rajnesh-Aspire-E5-573G>
2020-07-06 10:22:27 +0530

    Initial commit

---
+++ cp-abe/src/test/java/majorproject/cp_abe/AppTest.java
@@ -0,0 +1,20 @@
+package majorproject.cp_abe;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
